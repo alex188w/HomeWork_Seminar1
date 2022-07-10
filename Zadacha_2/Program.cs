0b231f098ce054587e610b80de493c191883a3f5
Алексей Шевченко <alex18w@mail.ru>
2022-07-10 23:05:07 +0500

    Задача 2

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число и нажмите клавишу Enter: ");
+int Number1 =  Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число и нажмите клавишу Enter: ");
+int Number2 =  Convert.ToInt32(Console.ReadLine());
+
+if (Number2 > Number1)
+{
+    Console.WriteLine("Число - " + Number1 + " является наменьшим");
+    Console.WriteLine("Число - " + Number2 + " является набольшим");
+}
+
+else
+{
+    Console.WriteLine("Число" + Number2 + "является наменьшим");
+    Console.WriteLine("Число" + Number1 + "является набольшим");   
+}
+
