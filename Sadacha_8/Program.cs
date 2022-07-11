0d61ea95f2cec78d51a15f206a69a8864ca081f4
Алексей Шевченко <alex18w@mail.ru>
2022-07-11 22:38:04 +0500

    Задача 8

---
+++ Sadacha_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число и нажмите клавишу Enter: ");
+int Number1 =  Convert.ToInt32(Console.ReadLine());
+int start = 2;
+
+    while (start <= Number1)
+    {
+    int Remaind = (start % 2);
+       if (Remaind == 0)
+       {
+       Console.Write(start + ", ");
+       }
+           
+       start++; //увеличиваем start на единицу
+    }
+Console.WriteLine();
