e279437288124e964e9f55b244b33df5e5f9f271
Алексей Шевченко <alex18w@mail.ru>
2022-07-11 21:50:57 +0500

    Задача 6

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число для проверки и нажмите клавишу Enter: ");
+int Number1 =  Convert.ToInt32(Console.ReadLine());
+int Remaind = (Number1 % 2);
+
+if (Remaind == 0)
+{
+    Console.WriteLine("Число - " + Number1 + " является четным");
+    
+}
+
+else
+{
+    Console.WriteLine("Число - " + Number1 + " является нечетным");
+}
